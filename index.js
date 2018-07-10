7162aa7d9f4b32ddf33aaf37549e91e4c59663c5
Nikolay Mihaylov <nmi@nb-syngeni379.local>
2018-07-10 12:21:31 +0200

    period extraction & workflow loop

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const parse = require('csv-parse');
+const stringify = require('csv-stringify');
 const date = require('date-and-time');
 const stoxx = require('./modules/stoxx');
 
@@ -17,14 +18,13 @@ const INDICES = {
   TICKER_OUT: 4
 };
 
-const DAYS_RANGE = 100;
+const DAYS_RANGE = 101;
 
 const EUROSTOXX_TICKER = "SX5E";
 const VIX_TICKER = "VIX";
 
-let eurostoxx = {};
-let vix = {};
 let indices = [];
+let retryCounter = 3;
 
 
 function prepareIndices(rows) {
@@ -34,24 +34,81 @@ function prepareIndices(rows) {
   }
 }
 
-function collectIndicesData() {
-  console.log(eurostoxx["Meta Data"]);
+function collectIndicesData(ind, out) {
+  out = out || false;
+  //index in
+  let index_name = (out) ? indices[ind][INDICES.COMPANY_OUT] : indices[ind][INDICES.COMPANY_IN];
+  let index = (out) ? indices[ind][INDICES.TICKER_OUT] : indices[ind][INDICES.TICKER_IN];
+  //date
+  let date_in_out = date.format(indices[ind][INDICES.DATE], DATE_OUT);
+
+
+  console.log("Collecting data for:", index_name);
+  stoxx.getData(index, (data, err) => {
+    if (data) {
+      console.log("Preparing data for:", index_name);
+      let csvArray = stoxx.getAdjustedCloseCombined({name: index_name, data}, date_in_out, DAYS_RANGE);
+      console.log("Done");
+      console.log("Writing data to file", index_name+".csv");
+      writeCsv(index_name, csvArray, date_in_out, (out) ? "OUT": "IN");
+    } else {
+      console.log("No data found for", index_name, "("+index+")");
+      console.log(err);
+      if (err.indexOf("Invalid API call") > -1 && retryCounter) {
+        console.log("Trying again...");
+        if (!out) ind--;
+        out = !out;
+        retryCounter--;
+      } else {
+        retryCounter = 3;
+      }
+    }
+
+    console.log("\n##########################");
+    console.log("Progress: " + (ind+1) + "/" + indices.length);
+    console.log("##########################\n\n");
+    //Delay due to api calls quotas
+    setTimeout(_ => {
+      if (out) {
+        if (ind < indices.length - 1) collectIndicesData(ind+1, false);
+        else console.log("All data extracted!");
+      } else {
+        collectIndicesData(ind, true);
+      }
+    }, 15000);
+  });
+
+}
+
+function writeCsv(name, data, dateString, inout) {
+
+  stringify(data, { delimiter: ";" }, (err, output) => {
+    if (err) throw err;
+    fs.writeFile("output/"+name+'_'+inout+'_'+dateString+'.csv', output, (err) => {
+      if (err) throw err;
+      //console.log(name+'.csv saved.');
+    });
+  });
 }
 
 
 //load data indices
-fs.readFile(PATH_INDICES, function (err, fileData) {
-  parse(fileData, {delimiter: ";"}, function(err, rows) {
-    // Your CSV data is in an array of arrys passed to this callback as rows.
-    prepareIndices(rows);
-    indices = rows;
-    console.log("Indices initianlised", indices);
-  })
-});
+function loadIndices() {
+  fs.readFile(PATH_INDICES, function (err, fileData) {
+    parse(fileData, {delimiter: ";"}, function(err, rows) {
+      // Your CSV data is in an array of arrys passed to this callback as rows.
+      prepareIndices(rows);
+      indices = rows;
+      console.log("Indices loaded");
+      collectIndicesData(1);
+    })
+  });
+}
 
 //load data indices
 fs.readFile(PATH_EUROSTOXX, function (err, fileData) {
   parse(fileData, {delimiter: ","}, function(err, rows) {
-    stoxx.init(["VIX", {"EUROSTOXX": rows}]);
+    stoxx.init(["VIX", {"EUROSTOXX": rows}], loadIndices);
+
   })
 });
--- modules/stoxx.js
+++ modules/stoxx.js
@@ -1,7 +1,7 @@
 const https = require('https');
 const config = require('./../privateConfig');
 
-const STOXX_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$TICKER&outputsize=full&apikey=" + config.API_KEY;
+const STOXX_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=$TICKER&outputsize=full&apikey=" + config.API_KEY;
 
 const DATA_PATTERNS = {
   ALPHAVANTAGE: {
@@ -21,31 +21,32 @@ const DATA_PATTERNS = {
     adjustedClose: 5,
     volume: 6
   }
-}
+};
 
 var stoxx = {};
 
 let indexes = {};
 
-function init(inxs) {
+function init(inxs, callback) {
   let counter = inxs.length;
 
   inxs.forEach(function (index, i) {
     if (typeof index === 'string') {
       getData(index, data => {
-        indexes[index] = prepareDataAlpha(data);
+        indexes[index] = data;
         counter--;
-        if (!counter) printStoxxReady();
+        if (!counter) stoxxReady(callback);
       });
     } else {
       indexes[Object.keys(index)[0]] = prepareDataEurostoxx(index[Object.keys(index)[0]]);
       counter--;
-      if (!counter) printStoxxReady();
+      if (!counter) stoxxReady(callback);
     }
   });
 }
 
 function prepareDataAlpha(data) {
+  if (!data) return data;
   let pattern = DATA_PATTERNS.ALPHAVANTAGE;
   Object.keys(data).forEach(function (k) {
     let current = {};
@@ -62,8 +63,8 @@ function prepareDataAlpha(data) {
 function prepareDataEurostoxx(data) {
   let pattern = DATA_PATTERNS.EUROSTOXX;
   let result = {};
-  data.forEach(function (k) {
-    result[k[pattern.data]] = {
+  data.forEach( k => {
+    result[k[pattern.date]] = {
       open: k[pattern.open],
       high: k[pattern.high],
       low: k[pattern.low],
@@ -71,6 +72,50 @@ function prepareDataEurostoxx(data) {
       adjustedClose: k[pattern.adjustedClose],
       volume: k[pattern.volume]
     }
+  });
+
+  return result;
+}
+
+function getAdjustedCloseForPeriod(stock, dateString, daysBefore, daysAfter) {
+  let days = Object.keys(stock);
+  let dateIndex = days.indexOf(dateString);
+  let dateFrom = dateIndex - daysBefore;
+  if (dateFrom < 0) {
+    dateFrom = 0;
+    console.warn("WARN", "Not enough data for", daysBefore, "days before", dateString, "! Taking", dateIndex, "days.");
+  }
+
+  let dateTo = dateIndex + daysAfter;
+  if (dateTo >= days.length) {
+    dateTo = days.length - 1;
+    console.warn("WARN", "Not enough data for", daysBefore, "days after", dateString, "! Taking", (dateTo - dateIndex), "days.");
+  }
+
+  let period = days.slice(dateFrom, dateTo);
+  let adjClose = period.map( day => {
+    return [day, convertToDecimalComma(stock[day].adjustedClose)];
+  });
+
+  return adjClose;
+}
+
+function convertToDecimalComma(num) {
+  return (num+"").replace(".", ",");
+}
+
+function getAdjustedCloseCombined(stock, dateString, interval) {
+  console.log("Extracting data for",stock.name);
+  let stockAdjClose = getAdjustedCloseForPeriod(stock.data, dateString, interval, interval);
+  console.log("Extracting data for EUROSTOXX");
+  let eurostoxxAdjClose = getAdjustedCloseForPeriod(indexes.EUROSTOXX, dateString, interval, interval);
+  console.log("Extracting data for VIX");
+  let vixAdjClose = getAdjustedCloseForPeriod(indexes.VIX, dateString, interval, interval);
+
+  let result = [["Date", "EUROSTOXX", "VIX", stock.name]];
+  stockAdjClose.forEach(function(e, i) {
+
+    result.push([stockAdjClose[i][0], eurostoxxAdjClose[i][1], vixAdjClose[i][1], stockAdjClose[i][1]]);
   });
 
   return result;
@@ -89,8 +134,14 @@ function getData(ticker, callback) {
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
       let result = JSON.parse(data);
+
       console.log("Loaded!");
-      callback(result["Time Series (Daily)"]);
+      if (result["Time Series (Daily)"]) {
+        callback(prepareDataAlpha(result["Time Series (Daily)"]));
+      } else {
+        callback(0, result["Error Message"]);
+      }
+
     });
 
   }).on("error", (err) => {
@@ -99,11 +150,13 @@ function getData(ticker, callback) {
 
 }
 
-function printStoxxReady() {
+function stoxxReady(cb) {
   console.log("Stoxx initialised", Object.keys(indexes));
+  cb();
 }
 
 stoxx.getData = getData;
 stoxx.init = init;
+stoxx.getAdjustedCloseCombined = getAdjustedCloseCombined;
 
 module.exports = stoxx;
